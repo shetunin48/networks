c28c9b6300771090fac9643cd3dee1ce110c76ac
shetunin48 <shetunin48@yandex.ru>
2024-04-06 03:22:45 +0300

    fix_all

--- execute_client.sh
+++ execute_client.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 make -s
-./server_client.o 8080 -p TCP --client 8081
+./server_client.o 8080 -p TCP --client 127.0.0.1
 make clean -s
--- server_client.c
+++ server_client.c
@@ -12,11 +12,11 @@
 #include <stdlib.h>
 #include <sys/wait.h>
    
-void fill_info(struct sockaddr_in* sock, int PORT) {
+void fill_info(struct sockaddr_in* sock, int PORT, char *ip) {
     memset(sock, 0, sizeof(struct sockaddr_in));
     sock->sin_family = AF_INET; 
     sock->sin_port = htons(PORT);
-    sock->sin_addr.s_addr = INADDR_ANY;
+    inet_aton(ip, (struct in_addr*)&sock->sin_addr.s_addr);
 }
 
 void TCPserver(int sockfd, struct sockaddr_in *client) {
@@ -35,8 +35,8 @@ void TCPserver(int sockfd, struct sockaddr_in *client) {
     }
     char s[256];
 
-    write(connfd_in, "Hello, I'm server!", 19);
     read(connfd_in, s, 255);
+    write(connfd_in, "Hello, I'm server!", 19);
     printf("I'm server, I recieved \"%s\"\n", s);
     close(connfd_in);
 }
@@ -59,7 +59,7 @@ void UDPserver(int sockfd, struct sockaddr_in *client) {
     int len = sizeof(*client);
 
     printf("I'm listenning UDP\n");
-    if (recvfrom(sockfd, s, sizeof(s), 0, (struct sockaddr*)&client, &len) < 0){
+    if (recvfrom(sockfd, s, sizeof(s) - 1, 0, (struct sockaddr*)&client, &len) < 0){ // Maybe here is overflow?)
         perror("ERROR recvfrom");
         close(sockfd); 
         exit(-1); 
@@ -91,6 +91,7 @@ void UDPcli(int sockfd, struct sockaddr_in *server) {
 
 int main(int argc, char const* argv[]) 
 {
+    char ip[256] = "127.0.0.1"; // DEFAULT
     int PORT1, PORT2;
     int type = SOCK_STREAM;
     int protocol = 0;
@@ -100,9 +101,9 @@ int main(int argc, char const* argv[])
             printf("\
                         This is client and server program in one. To use it you should \n\
                     specify PORT which is server port. If You want to execute client use \n\
-                    --client and PORT which is client port. You can also specify -p or --protocol \n\
+                    --client with IP. You can also specify -p or --protocol \n\
                     for \"UDP\" or \"TCP\" (default is TCP).\
-                    \n");
+                    \n\n");
             exit(0);
         }
         if(( strcmp("-p", argv[i]) == 0 || strcmp("--protocol", argv[i]) == 0) && i != argc - 1) {
@@ -115,7 +116,7 @@ int main(int argc, char const* argv[])
             }
         }
         if(strcmp("--client", argv[i]) == 0 && i != argc - 1) {
-            sscanf(argv[i + 1], "%d", &PORT2);
+            sscanf(argv[i + 1], "%s", ip);
             is_client = 1;
         }
     }
@@ -133,11 +134,10 @@ int main(int argc, char const* argv[])
         exit(-1); 
     }
 
-    struct sockaddr_in server, client;
-    fill_info(&server, PORT1);
+    struct sockaddr_in server;
+    fill_info(&server, PORT1, ip);
 
     if(is_client) { // client
-        fill_info(&client, PORT2);
         if(type == SOCK_STREAM) { // client TCP
             TCPcli(sockfd, &server);
         }
@@ -147,11 +147,11 @@ int main(int argc, char const* argv[])
         close(sockfd); 
     }
     else { // server
+        struct sockaddr_in client;
         int option = 1;
         setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &option, sizeof(option));
         if (bind(sockfd, (struct sockaddr *)&server, sizeof(server)) != 0) { 
             perror("ERROR bind");
-            close(sockfd); 
             close(sockfd); 
             exit(-1); 
         }
